fae9b37df7903c2c4fe1b59e9344248aa9177355
qouc-tahi <ct1.nquocthai@gmail.com>
2024-05-27 13:38:01 +0700

    update axios config

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_BASE_API_URL,
-  timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+  timeout: 30 * 1000,
 });
 
 export default instance;
